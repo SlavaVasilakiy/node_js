1124f43b90694ac12b0f53a83a1cce09d33aa026
SlavaVasilakiy <slk55slk@gmail.com>
2024-01-12 16:14:47 +0300

    Homework 4 add comments to code and add README.md file

--- Homework_4/app.js
+++ Homework_4/app.js
@@ -1,15 +1,33 @@
+// Подключение модуля express для создания сервера
 const express = require('express');
+
+// Подключение функций-обработчиков запросов из модуля handlers
 const {getAllUsers, getUserById, createUser, updateUser, deleteUser} = require('./handlers');
 
+// Создание экземпляра приложения express
 const app = express();
+
+// Использование middleware express.json() для обработки JSON-запросов
 app.use(express.json());
 
+// Установка обработчиков маршрутов для различных HTTP-запросов
+
+// Обработка GET-запроса к /users вызывает функцию getAllUsers из модуля handlers
 app.get('/users', getAllUsers);
+
+// Обработка GET-запроса к /users/:id вызывает функцию getUserById из модуля handlers
 app.get('/users/:id', getUserById);
+
+// Обработка POST-запроса к /users вызывает функцию createUser из модуля handlers
 app.post('/users', createUser);
+
+// Обработка PUT-запроса к /users/:id вызывает функцию updateUser из модуля handlers
 app.put('/users/:id', updateUser);
+
+// Обработка DELETE-запроса к /users/:id вызывает функцию deleteUser из модуля handlers
 app.delete('/users/:id', deleteUser);
 
+// Прослушивание порта 3000 и вывод сообщения в консоль при успешном запуске сервера
 app.listen(3000, () => {
 	console.log('Server is running on port 3000');
 });
--- Homework_4/handlers.js
+++ Homework_4/handlers.js
@@ -1,29 +1,40 @@
+// Подключение модуля fs для работы с файлами и его промисификация
 const fs = require('fs').promises;
 
+// Функция getUsers считывает данные о пользователях из файла
 const getUsers = async () => {
 	try {
-		const users = await fs.readFile('users.json', 'utf-8');
-		return JSON.parse(users);
+		// Чтение содержимого файла 'users.json' в формате utf-8
+		const usersData = await fs.readFile('users.json', 'utf-8');
+		// Преобразование данных из JSON строки в объект JavaScript
+		return JSON.parse(usersData);
 	} catch (error) {
+		// В случае ошибки при чтении файла, вывод сообщения об ошибке и возврат пустого массива
 		console.error('Error reading users:', error.message);
 		return [];
 	}
 };
 
+// Функция saveUsers записывает данные о пользователях в файл 'users.json'
 const saveUsers = async (users) => {
 	try {
+		// Преобразование массива пользователей в форматированную JSON строку с отступами
 		const usersString = JSON.stringify(users, null, 2);
+		// Запись данных в файл 'users.json' с указанием кодировки utf-8 и флага w (запись)
 		await fs.writeFile('users.json', usersString, {encoding: 'utf-8', flag: 'w'});
 	} catch (error) {
+		// В случае ошибки при записи данных в файл, вывод сообщения об ошибке
 		console.error('Error saving users:', error.message);
 	}
 };
 
+// Функция getAllUsers обрабатывает GET-запрос, возвращая список всех пользователей
 const getAllUsers = async (req, res) => {
 	const users = await getUsers();
 	res.send(users);
 };
 
+// Функция getUserById обрабатывает GET-запрос с указанным id, возвращая пользователя или статус 404, если не найден
 const getUserById = async (req, res) => {
 	const users = await getUsers();
 	const user = users.find((user) => user.id === parseInt(req.params.id));
@@ -36,6 +47,7 @@ const getUserById = async (req, res) => {
 	res.send(user);
 };
 
+// Функция createUser обрабатывает POST-запрос, добавляя нового пользователя и возвращая его
 const createUser = async (req, res) => {
 	const users = await getUsers();
 	const newUser = req.body;
@@ -44,6 +56,7 @@ const createUser = async (req, res) => {
 	res.send(newUser);
 };
 
+// Функция updateUser обрабатывает PUT-запрос с указанным id, обновляя данные пользователя и возвращая обновленного пользователя
 const updateUser = async (req, res) => {
 	const users = await getUsers();
 	const userId = parseInt(req.params.id);
@@ -61,6 +74,7 @@ const updateUser = async (req, res) => {
 	res.send(users[userIndex]);
 };
 
+// Функция deleteUser обрабатывает DELETE-запрос с указанным id, удаляя пользователя и возвращая пустой объект
 const deleteUser = async (req, res) => {
 	const users = await getUsers();
 	const userId = parseInt(req.params.id);
@@ -76,4 +90,5 @@ const deleteUser = async (req, res) => {
 	res.send({});
 };
 
+// Экспорт функций для использования в других модулях
 module.exports = {getAllUsers, getUserById, createUser, updateUser, deleteUser};
